3500b656d679e05b11311c964a3221cc23e9a7b2
Sascha Maassen <sascha.maassen@uni-due.de>
2019-06-16 10:57:51 +0200

    Made PointInQ4 saver by boolean tests

--- include/ELSE_GeometricLibrary.hpp
+++ include/ELSE_GeometricLibrary.hpp
@@ -9,6 +9,20 @@ namespace ELSE {
 namespace Geometric {
 
   inline bool PointInQ4(double X1[3], double X2[3], double X3[3], double X4[3], double XP[3]){
+    /*
+    This function determines wether a 3d point XP[3] is inside (including the boundary) or outside a
+    plane, yet defined by 3d Coordinates quadrilateral element Q4.
+    The element is defined by four nodes, three coordinates each: X1,..,X4
+    functions return:
+                        true  -> if XP in B or on dB
+                        false -> else
+    Procedural detals:
+    The function first determines inward pointing normals on each side of the element.
+    Then it computes the vector pointing from the first node of each side to the point XP (testvectors).
+    The point XP lies in direction of the inward pointing normal if the scalar product of the normal and the testvector
+    is greater equal 0.
+    This test is performed for each side of the element. If each test returns true, the point is inside the element.
+    */
     bool DetailedOutput = false;
     if (DetailedOutput){
     std::cout << "Test X1: " << X1[0] << " ," << X1[1] << " ," << X1[2] << std::endl;
@@ -58,26 +72,18 @@ namespace Geometric {
     std::cout << "TestVector X3XP: " << tv[2][0] << ", " << tv[2][1] << ", " << tv[2][2] << ", " << std::endl;
     std::cout << "TestVector X4XP: " << tv[3][0] << ", " << tv[3][1] << ", " << tv[3][2] << ", " << std::endl;
     }
-    // compute sum of scalar products to comare with abs of scalar products
-    double sumabs = 0.0;
-    double sum    = 0.0;
-    for(int i=0;i<4;i++){
-    //std::cout << "ScalarProduct: " << ( tv[i][0]*n[i][0] + tv[i][1]*n[i][1] + tv[i][2]*n[i][2] ) << std::endl;
-          sumabs  += abs( tv[i][0]*n[i][0] + tv[i][1]*n[i][1] + tv[i][2]*n[i][2] );
-          sum     += ( tv[i][0]*n[i][0] + tv[i][1]*n[i][1] + tv[i][2]*n[i][2] );
-    }
-    bool XPInside = (sumabs-sum < 10e-10)? true : false;
-    if (DetailedOutput){
-    std::cout << "sum      : " << sum << std::endl;
-    std::cout << "sumabs   : " << sumabs << std::endl;
-    if(XPInside) {
-        std::cout << "XPInside : True" << std::endl;
-      } else {
-        std::cout << "XPInside : False" << std::endl;
-      }
-    }
-    return XPInside;
-  };
+
+    // perform tests
+    if ( (tv[0][0]*n[0][0] + tv[0][1]*n[0][1] + tv[0][2]*n[0][2]) >=10e-12 ){
+      if ( (tv[1][0]*n[1][0] + tv[1][1]*n[1][1] + tv[1][2]*n[1][2]) >=10e-12 ){
+        if ( (tv[2][0]*n[2][0] + tv[2][1]*n[2][1] + tv[2][2]*n[2][2]) >=10e-12 ){
+          if ( (tv[3][0]*n[3][0] + tv[3][1]*n[3][1] + tv[3][2]*n[3][2]) >=10e-12 ){
+            return true;
+          } else {return false;}
+        } else {return false;}
+      } else {return false;}
+    } else {return false;}
+};
 
 
 } // namespace Geometric
